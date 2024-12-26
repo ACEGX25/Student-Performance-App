3010b949630c34af72ed771caaf6295ee581397c
SILLYCAT <143728245+ACEGX25@users.noreply.github.com>
2024-12-26 16:42:01 +0530

    haha

---
+++ Backend/src/main/java/com/student_per_app/student_performace_application/StudentPerformaceApplication.java
@@ -0,0 +1,13 @@
+package com.student_per_app.student_performace_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPerformaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPerformaceApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/student_per_app/student_performace_application/StudentPerformaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student_per_app.student_performace_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPerformaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
